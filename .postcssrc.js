87785ce11d450e3e512c8401736e9f6fb2a6a0d3
Robert Forbes <robert@roboticdinosaur.co.uk>
2021-02-09 12:57:17 +0000

    Initial commit for landing page (laptop)

--- .postcssrc.js
+++ .postcssrc.js
@@ -12,10 +12,7 @@ const postcssConfig = {
 			]
 		}),
 		postcssPresetEnv({
-			stage: 1,
-			features: {
-				'nesting-rules': true,
-			}
+			stage: 0,
 		}),
 		postcssReporter({
 			clearReportedMessages: true
