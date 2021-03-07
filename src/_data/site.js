c4fd729a71de9c06cc59f30b44d6ea124b5f4be4
Robert Forbes <robert@roboticdinosaur.co.uk>
2021-03-07 09:01:30 +0000

    add base url

---
+++ src/_data/site.js
@@ -0,0 +1,11 @@
+module.exports = {
+	"name": "loopstudios",
+	"logo": "logo.svg",
+	"shortDescription": "",
+	"url": process.env.URL || "https://rfd-loopstudios.netlify.app",
+	"analytics": {
+		"enabled": false,
+		"gaid": "X-XXXXXXXXXX"
+	},
+	"maxProjectsPerPage": 10
+}
